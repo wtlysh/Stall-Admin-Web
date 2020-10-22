16d55bd8a9c53b6d41f99ef212642ffeacaa1b5f
wtlysh <2840310962@qq.com>
2020-10-22 21:57:29 +0800

    首页简单实现

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
   ],
   parserOptions: {
     parser: 'babel-eslint'
@@ -13,6 +12,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'semi': 0
+    'semi': 0,
+    "space-before-function-paren": 0
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,62 @@
 <template>
-  <div id="home" class="absolute flex"></div>
+  <div id="home" class="absolute">
+    <el-container class="">
+      <el-header style="height: 100px" class="border bg-white text-center mb-4"
+        >城市地摊片区规划及地摊线上综合管理系统</el-header
+      >
+      <el-tabs type="card">
+        <el-tab-pane label="地摊片区规划大数据分析服务"
+          ><el-container class="">
+            <el-container class="">
+              <el-main class="bg-white border">Main</el-main>
+            </el-container>
+            <el-aside width="200px" class="bg-white border pl-2"
+              >Aside</el-aside
+            >
+          </el-container></el-tab-pane
+        >
+        <el-tab-pane label="地摊线上综合管理系统">
+          <el-container class="">
+            <el-aside width="200px" class="bg-white border pr-2"
+              >Aside</el-aside
+            >
+            <el-container class="">
+              <el-main class="bg-white border">Main</el-main>
+            </el-container>
+          </el-container>
+        </el-tab-pane>
+      </el-tabs>
+    </el-container>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
-<style lang="scss" scoped>
+<style lang="scss" >
+#home {
+  top: 15%;
+  left: 15%;
+  width: 70%;
+  .el-header {
+    color: #333;
+    line-height: 100px;
+    font-size: 25px;
+  }
+  .el-tabs__nav {
+    width: 100%;
+    background-color: white;
+  }
+  .el-tabs__item {
+    height: 80px;
+    font-size: 22px;
+    width: 50%;
+    line-height: 80px;
+    text-align: center;
+  }
+}
 </style>
